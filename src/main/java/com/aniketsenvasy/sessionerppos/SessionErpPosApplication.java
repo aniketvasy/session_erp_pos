8ab7ac5bb21e51265df92154bf58a725ec605ff4
aniketvasy <aniketsen.vasyerp@gmail.com>
2023-06-26 19:34:49 +0530

    react added to spring boot with same origin

---
+++ src/main/java/com/aniketsenvasy/sessionerppos/SessionErpPosApplication.java
@@ -0,0 +1,13 @@
+package com.aniketsenvasy.sessionerppos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionErpPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionErpPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aniketsenvasy/sessionerppos/SessionErpPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniketsenvasy.sessionerppos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionErpPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
