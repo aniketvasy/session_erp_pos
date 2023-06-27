52f3535f17ef5ecf0ec5d32820e557cfd4865486
aniketvasy <aniketsen.vasyerp@gmail.com>
2023-06-27 19:46:06 +0530

    GEN-459 | Configured Spring routing for JSP, created React app with MUI, integrated React Router.

--- src/main/java/com/aniketsenvasy/sessionerppos/SessionErpPosApplication.java
+++ src/main/java/com/aniketsenvasy/sessionerppos/SessionErpPosApplication.java
@@ -1,13 +1,45 @@
 package com.aniketsenvasy.sessionerppos;
 
+import java.time.Instant;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
-public class SessionErpPosApplication {
+public class SessionErpPosApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SessionErpPosApplication.class, args);
+	}
+	
+//	@Primary
+//	@Bean
+//	public TaskExecutor primaryTaskExecutor() {
+//		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//		// add necessary properties to the executor
+//		return executor;
+//	}
+
+	
+//	@PostConstruct
+//	public void init() {
+//		Instant instant = Instant.now();
+//		System.out.println(instant.toString());
+//		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata")); // It will set UTC timezone
+//	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SessionErpPosApplication.class);
 	}
 
 }
--- src/main/java/com/aniketsenvasy/sessionerppos/controller/ClientForwardController.java
+++ src/main/java/com/aniketsenvasy/sessionerppos/controller/ClientForwardController.java
@@ -2,12 +2,22 @@ package com.aniketsenvasy.sessionerppos.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
 public class ClientForwardController {
 
-//	@GetMapping(value = "/**/{path:[^\\.]*}")
-//	public String forward() {
-//		return "forward:/";
-//	}
+    @GetMapping(value = { "/", "" })
+    public String home() {
+    	System.err.println("root");
+        return "index";
+    }
+
+    @GetMapping(value = "/pos/**")
+    public ModelAndView forward() {
+    	System.err.println("pos");
+        return new ModelAndView("forward:/index.html");
+    }
 }
